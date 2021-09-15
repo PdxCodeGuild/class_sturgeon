2182f0a8dc481f33fcbe1fe8227a99f54845d4de
Elliott smith <elliott.smith2437@gmail.com>
2021-09-15 16:33:47 -0700

    lab 3

---
+++ code/elliott/lab_03.py
@@ -0,0 +1,17 @@
+
+
+word_converter = {
+    1: 'one', 2: 'two', 3: 'four', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight',
+    9: 'nine', 10: 'ten', 11: 'eleven', 12: 'twelve', 13: ' thriteen', 14: 'fourteen',
+    16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen', 20: 'twenty',
+    30: 'thirty', 40: 'fourty', 50: 'fifty', 60: 'sixty', 70: 'eighty', 90: 'ninety'
+
+
+}
+
+num = int(input('Pick a number from 0 to 999 that you want to convert to english. '))
+
+
+def final_conv(word_converter, num):
+
+    # print(word_converter[num])
