f0eb909d011233679dade523f1588304ae7d6235
M <mannygalvan@Manuels-MacBook-Pro.local>
2021-09-15 16:34:22 -0700

    Lab 3 commit, not done

---
+++ code/manny/lab03.py
@@ -0,0 +1,8 @@
+"""Lab 3"""
+
+x = int(input("Please input a number between 0-99 and I'll tell you how it's written out in English! "))
+
+
+if isinstance(x, int): 
+    print("Thank you!")
+    pass
