ad99c6d9e718106e31cd020618ecfd6fa40d9037
Ian <iandavis6481@gmail.com>
2021-09-20 18:34:51 -0700

    lab 07 fix

--- code/ian/lab07v2.py
+++ code/ian/lab07v2.py
@@ -1,28 +1,25 @@
-# establish list with starting index
 english = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 # ask for input letter and convert to list
-letters_in = list(input('Enter a message: '))
-rot = int(input('Level of security: '))
+message_in = list(input('Enter secret message: '))
+rot = int(input('Security Level: '))
 
+m_i_list = []
+for letter in message_in:
+    letter_index = m_i_list.append(english.index(letter))
 
-letters_index_list = []
+print(m_i_list)
 
-for l in letters_in:
-    
-    letters_index = letters_index_list.append(english.index(l))
-    
+message_out = []
 
+for num in m_i_list:
 
-message = []
-for num in letters_index_list:
     try:
         new_letter = english[num + rot]
-        message.append(new_letter)
+        message_out.append(new_letter)
     except IndexError:
-        new_letter = english[num - rot]
-        message.append(new_letter)
+        new_letter = english[num + rot - 26]
+        message_out.append(new_letter)
 
-message = ''.join(message)
-print(message)
-
+message_out = ''.join(message_out)      
+print(message_out)
---
+++ code/ian/lab08.py
@@ -0,0 +1,5 @@
+data = [1, 2, 3, 4, 5, 4, 3, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8]
+
+for x in data:
+    if data[x] > data[x+1]:
+        print({data[x]})
