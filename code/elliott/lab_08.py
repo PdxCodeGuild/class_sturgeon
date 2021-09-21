fa6b4d186452053d69adca9f4dc1d81e995bd4ca
Elliott smith <elliott.smith2437@gmail.com>
2021-09-20 20:49:12 -0700

    lab 08

--- code/elliott/lab_08.py
+++ code/elliott/lab_08.py
@@ -1,21 +1,30 @@
-
 a = input('enter your data: ')
 data = [int(x) for x in a]
 
 
-# def Peaks(data):
 peaks_list = []
 
-for i in data[1:-1]:  # the pre and post comparison means I have to start from item 2 to second last item
-    pre = data.index()
-    post = data.index(2)
-    if i > data[pre] and i > data[post]:
-        peaks_list.append(data.index(i))
-   # return peaks_list
 
+for i, element in enumerate(data):
+    previous_element = data[i-1] if i > 0 else 0
+    current_element = element
+    next_element = data[i+1] if i < len(data)-1 else 10
+    if previous_element < current_element > next_element:
+        peaks_list.append(i)
 
 print(peaks_list)
+valley_list = []
 
+for i, element in enumerate(data):
+    previous_element = data[i-1] if i > 0 else 0
+    current_element = element
+    next_element = data[i+1] if i < len(data)-1 else 0
+    if previous_element > current_element < next_element:
+        valley_list.append(i)
+
+print(valley_list)
+
+print(peaks_list + valley_list)
 
 '''
 >>> data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
@@ -28,17 +37,4 @@ print(peaks_list)
 
 123456765456789876789 #data
 
-'''
-
-'''
-mylist = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
-min = []
-for i in mylist[1:-1]:  # the pre and post comparison means I have to start from item 2 to second last item
-    pre = data.index(i)-1
-    post = data.index(i)+1
-
-    if i < mylist[pre] and i < mylist[post]:
-
-        min.append(mylist.index(i))
-    print('min=', min)
 '''
