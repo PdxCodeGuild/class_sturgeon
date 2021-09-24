f5cf3267e8a4241e1260310454fceb05306268c2
Leonardo De Oliveira <89034569+lenehrt@users.noreply.github.com>
2021-09-24 19:42:16 -0400

    correction on Lab 8

--- code/leo/Labs/Lab_8_Peaks_and_Valleys.py
+++ code/leo/Labs/Lab_8_Peaks_and_Valleys.py
@@ -20,6 +20,18 @@ def valley(data): #valley list
          
     return valley
 
+def peak_and_valley(data):
+    peak_and_valley = []
+
+    for index in range(len(data) - 1): #add -1 to avoid end of list error
+        if (data[index] > (data[index - 1]) and data[index] > data[index + 1]) or (data[index] < (data[index - 1]) and data[index] < data[index + 1]): 
+            # if item is bigger than item location -1 and item location +1
+            peak_and_valley.append(index)
+    peak_and_valley.pop(0)
+    return peak_and_valley
+
+
+
 data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
 
 print(f'''
@@ -30,5 +42,5 @@ Valley index/es:
 {valley(data)}
 
 Peak and Valley index/es: 
-{peak(data) + valley(data)} 
+{peak_and_valley(data)} 
 ''')
