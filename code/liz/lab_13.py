0d89a1e1af746370caa3ad15b15e0b60814006ce
liz <bean@lizs-MacBook-Pro.local>
2021-09-29 17:34:36 -0700

    lab 13

--- code/liz/lab_13.py
+++ code/liz/lab_13.py
@@ -2,7 +2,7 @@
 
 
 class Player:
-    def __init__ (self, t, n):
+    def __init__ (self, n, t):
         self.n = n
         self.t = t
 
@@ -17,12 +17,30 @@ class Game:
     
     def __repr__(self):
         for i in self.b:
-            return '|'.join(i)
+            print('|'.join(i))
     
     def move (self, x, y, player):
         self.b[y][x] = player.t
 
-    #def calc_winner (self):
+    def calc_winner (self, player):
+        if self.b[0][0] == self.b[1][0] == self.b[2][0] == player.t:
+            return player.t
+        elif self.b[0][1] == self.b[1][1] == self.b[2][1] == player.t:
+            return player.t
+        elif self.b[0][2] == self.b[1][2] == self.b[2][2] == player.t:
+            return player.t
+        elif self.b[0][0] == self.b[0][1] == self.b[0][2] == player.t:
+            return player.t
+        elif self.b[1][0] == self.b[1][1] == self.b[1][2] == player.t:
+            return player.t
+        elif self.b[2][0] == self.b[2][1] == self.b[2][2] == player.t:
+            return player.t
+        elif self.b[0][0] == self.b[1][1] == self.b[2][2] == player.t:
+            return player.t
+        elif self.b[0][2] == self.b[1][1] == self.b[2][0] == player.t:
+            return player.t
+        else:
+            return None
   
     
     def is_full (self):
@@ -31,35 +49,57 @@ class Game:
                 if j == ' ':
                     return False
         return True
+    
+    def is_game_over (self, player):
+        full_board = self.is_full()
+        someone_won = self.calc_winner(player)
+        if full_board or someone_won:
+            return True
 
 
 
 
-
-
+# start game!
 print('Welcome to Tic-Tac-Toe!')
 while True:
     game = Game()
     command = input("Would you like to play a game? 'yes' or 'no': ")
+
     if command == 'yes':
         name1 = input("Enter player 1's name: ")
-        token1 = input("What token would you like? 'X' or 'O': ")
+        token1 = input("What token would you like? 'X' or 'O': ").upper()
         name2 = input("Enter player 2's name: ")
-        if token1 == 'X':
-            token2 = 'O'
-        else:
-            token2 = 'X'
+        token2 = 'O' if token1 == 'X' else 'X'
         player1 = Player(name1, token1)
         player2 = Player(name2, token2)
-# get players to alternate turns
-# check and print out gameboard
+
+
+        counter = 0
+        while True:
+            current_player = player1 if counter % 2 == 0 else player2
+            counter += 1
+            move1 = int(input('What column would you like to place your token in: 0, 1, 2? '))
+            move2 =int(input('What row would you like to place your token in: 0, 1, 2? '))
+            game.move(move1, move2, current_player)
+            game.__repr__()
+            if game.is_game_over(current_player):
+                print('The game is over')
+                break
+
+
+        if game.is_full():
+            print('The board is full!')
+
+        for current_player in [player1, player2]:
+            if game.calc_winner(current_player):
+                print(f"{game.calc_winner(current_player)}'s won the game!")
 
 
     elif command == 'no':
         break
+    
     else:
         print('Entry not recognized')
 
     
 
-#game.__repr__()
