7fc766798d4a3e0f2240ac9ec8f16fb0e7c67e9c
M <mannygalvan@Manuels-MacBook-Pro.local>
2021-09-30 14:35:30 -0700

    Lab 6 update

---
+++ code/manny/lab13.py
@@ -0,0 +1,29 @@
+"""Lab 13"""
+
+#Make two classes, Player and Game and a main function
+#Player has two properties, name and token
+class Player:
+    def __init__(self):
+        name = self.name
+        token = self.token
+
+#game has one property, game has 5 methods
+class Game:    
+    def __repr__(self):
+        pass
+
+    def move(self):
+        pass 
+
+    def calc_winner(self):
+        pass
+
+    def is_full(self):
+        pass
+
+    def is_game_over(self):
+        pass 
+    
+#main function
+def main():
+    pass
