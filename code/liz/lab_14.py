0a32706e6e0a8436db081cb2131e71d67cb32462
liz <bean@lizs-MacBook-Pro.local>
2021-10-04 18:58:30 -0700

    lab 15

--- code/liz/lab_14.py
+++ code/liz/lab_14.py
@@ -6,12 +6,18 @@ import re
 
 
 while True:
-    search_term = input('What subject would you like dad jokes about? ')
+    search_term = input('What subject would you like dad jokes about? "e" to exit\n')
     response = requests.get('https://icanhazdadjoke.com/search?term=' + search_term, headers={'accept': 'application/json'})
-    # response = requests.get('https://icanhazdadjoke.com/', headers={'accept': 'application/json'})
     jokes = response.json()['results']
-    for joke in jokes:
-        print(joke['joke']+'\n')
+    if search_term == 'e':
+        break
+    else:
+        for joke in jokes:
+            print(joke['joke']+'\n')
+            another = input('Do you want to hear another one? "y" or "n"\n')
+            if another != 'y':
+                break
+            
     # joke_words = joke.split(' ')
     # joke = re.split('[?,.]', response.json()['joke'])
     #print(jokes)
