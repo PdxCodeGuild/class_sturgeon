d8dca61d8f860984f12e50303d88794b2942733c
Leonardo De Oliveira <89034569+lenehrt@users.noreply.github.com>
2021-10-07 19:39:01 -0400

    MiniCapstone Debugged

--- code/leo/Labs/Leo_HNT_APP.py
+++ code/leo/Labs/Leo_HNT_APP.py
@@ -2,7 +2,6 @@ import requests
 import datetime
 from statistics import mean
 import time
-import lenny
 
 
 def slowprint(text): # print txt slow
@@ -12,11 +11,11 @@ def slowprint(text): # print txt slow
 
 def address_url_converter(): #Asks user for Address and concatenated it into GoogleMaps URL format
     
-    slowprint(f'\n >>>>>>> Lets find the location you want point as the middle of your circle.\n         Please enter the address: (exp - 123 Rocket Rd) \n >>>>>>> ')
+    slowprint(f'\n >>>>>>> What is the location you want to place your Hotspot?.\n         Please enter the address: (exp - 123 Rocket Rd) \n >>>>>>> ')
     street = input()
-    slowprint(f'\n >>>>>>> OK!\n         Now, enter the city (exp - Los Angeles):\n >>>>>>> ')
+    slowprint(f'\n >>>>>>> Awesome!\n         Now, enter the City (exp - Los Angeles):\n >>>>>>> ')
     city = input()
-    slowprint(f'\n >>>>>>> Thanks!\n         Now, enter the state (exp - CA):\n >>>>>>> ')
+    slowprint(f'\n >>>>>>> Great!\n         Now, 2 letters US State (exp - CA):\n >>>>>>> ')
     state = input()
     
     return str(street.strip().title().replace(' ', '+') + ',+' + city.strip().title().replace(' ', '+') + ',+' + state.strip().upper().replace(' ', '+'))
@@ -30,12 +29,12 @@ def lat_log():#Uses address_url_converted to connect with GoogleMaps API, and re
         try:
             filtered_data = GoogleMaps_data['results'][0]['geometry']['location']
             url_lat_lon = str(f"lat={filtered_data['lat']}&lon={filtered_data['lng']}")
-            slowprint("\n\n>>>>>>> GOT IT!!!!  Please wait a little while I search the area... f(ಠ‿↼)z\n ")
+            slowprint("\n\n>>>>>>> GOT IT!!!!  Please wait a little while I search the area... (° ͟ʖ °)\n ")
             break
         except IndexError:
-            slowprint("\n >>>>>>> ಠ▄ಠ Duuuudddeeee!!!!!")
+            slowprint("\n >>>>>>> (° ͟ʖ °) Duuuudddeeee!!!!!")
             print(f'''\n\n          ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄\n          ████▌▄▌▄▐▐▌█████\n          ████▌▄▌▄▐▐▌▀████\n          ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀\n\n ''')
-            slowprint(f'''>>>>>>> Unfortunatelly GoogleMaps was not able to find the address in the format you enterred... (╯ ͠° ͟ʖ ͡°)╯┻━┻\n         please try again entering the address again, or enter another address closed by the locations.\n"''')
+            slowprint(f'''>>>>>>> Unfortunately GoogleMaps was not able to find the address in the format you entered... (╯ ͠° ͟ʖ ͡°)╯┻━┻\n\n         Let's try entering the address again or try a different address in the same area\n"''')
 
     return url_lat_lon
 
@@ -57,29 +56,33 @@ def get_reward(HotSpot_address): #Takes HotSpot Address in HNT format and return
 
 def HotSpots_Data_Finder():# Takes Lat and Log, Ask user for a radius distance, connect with HNY API, returns list with details about each HotStop in the radius
     # lat_log = 'lat=28.4947354&lon=-81.71927'   
-    slowprint(f'\n >>>>>>> Lets start by enterting the distance of your search... \n         Please enter the radius distance in --> miles <-- that you would like to the search.\n >>>>>>> How many miles? : ')   
+    slowprint(f'\n >>>>>>> Lets start by entering the distance (radius) of your wish to search. \n         Please enter how far in miles radius from your location you would like to search\n >>>>>>> How many miles? : ')   
     distance = str(int(float(input()) * 1609.34))
 
     find_hotspots_url = f'https://api.helium.io/v1/hotspots/location/distance?{lat_log()}&distance={distance}'
 
     hotspots_url = requests.get(f'{find_hotspots_url}')
-    HotSpots_data = hotspots_url.json()
-    HotSpots = HotSpots_data['data']
+    try:
+        HotSpots_data = hotspots_url.json() #  wrong address issue
+        HotSpots = HotSpots_data['data']
+        
+        OrganizedList = list()
 
-    OrganizedList = list()
+        for each_HotSpot in HotSpots:
+            HotSpot_name = each_HotSpot['name']
+            HotSpot_status = each_HotSpot['status']['online']
+            HotSpot_scale = each_HotSpot['reward_scale']
+            HotSpot_address = each_HotSpot['address']
+            HNT = get_reward(HotSpot_address)
 
-    for each_HotSpot in HotSpots:
-        HotSpot_name = each_HotSpot['name']
-        HotSpot_status = each_HotSpot['status']['online']
-        HotSpot_scale = each_HotSpot['reward_scale']
-        HotSpot_address = each_HotSpot['address']
-        HNT = get_reward(HotSpot_address)
+            eactHostSpotDict = {'Name': HotSpot_name.title(), 'Status': HotSpot_status.upper(), 'Reward Scale': HotSpot_scale, '24h' : HNT[0], 'Week' : HNT[1], 'Month' : HNT[2] }
 
-        eactHostSpotDict = {'Name': HotSpot_name.title(), 'Status': HotSpot_status.upper(), 'Reward Scale': HotSpot_scale, '24h' : HNT[0], 'Week' : HNT[1], 'Month' : HNT[2] }
+            OrganizedList.append(eactHostSpotDict)
 
-        OrganizedList.append(eactHostSpotDict)
+        return OrganizedList
 
-    return OrganizedList
+    except:
+        return 'zero'
 
 def turn_data_into_csv(data):# Turns data into .CSV 
 
@@ -110,47 +113,47 @@ def average(data):# Takes data print average and returns answer if user wants to
     #     {'Name': 'Innocent-Ash-Ferret', 'Status': 'OFFLINE', 'Reward Scale': 1.0, '24h': 0.51427221, 'Week': 0.8411644, 'Month': 0.8411644}, 
     #     {'Name': 'Clean-Myrtle-Corgi', 'Status': 'ONLINE', 'Reward Scale': 1.0, '24h': 0.01982604, 'Week': 0.01982604, 'Month': 4.33192843}
     #     ]
+    try:
+        ave_24h = list()
+        ave_week = list()
+        ave_month = list()
 
-    ave_24h = list()
-    ave_week = list()
-    ave_month = list()
+        for each in data:
+            if each['Status'] == 'ONLINE':
+                ave_24h.append(each['24h'])
+                ave_week.append(each['Week'])
+                if each['Week'] != each['Month']:
+                    ave_month.append(each['Month'])
+                
+        slowprint(f'''
+            Found {len(ave_24h)} HNT HotSpots currently 'Online' in the area.
 
-    for each in data:
-        if each['Status'] == 'ONLINE':
-            ave_24h.append(each['24h'])
-            ave_week.append(each['Week'])
-            if each['Week'] != each['Month']:
-                ave_month.append(each['Month'])
-            
-    slowprint(f'''
-        Found {len(ave_24h)} HotSpots currently 'Online' in the area.
+            The estimated average rewards 
 
-        The estimated average rewards 
+            ₿    {round(mean(ave_24h), 2)} HNT ₿ in the past 24hs
+            ₿₿₿   {round(mean(ave_week), 2)} HNT ₿ in the past 7 days
+            ₿₿₿₿₿  {round(mean(ave_month), 2)} HNT ₿ in the past 30 days
 
-          ₿    {round(mean(ave_24h), 2)} HNT ₿ in the past 24hs
-         ₿₿₿   {round(mean(ave_week), 2)} HNT ₿ in the past 7 days
-        ₿₿₿₿₿  {round(mean(ave_month), 2)} HNT ₿ in the past 30 days
+            Here are more details:  ↓            
+            ''')
 
-        Here are the more details:  ↓            
-        ''')
+        cc = 0
+        for each in data:
+            cc += 1
+            if each['Status'] == 'OFFLINE':
+                slowprint(f"\n    {cc} - {each['Name']} is currently OFFLINE\n\n")
+            else:
+                print(f"\n    {cc} - {each['Name']} is currently ONLINE with a Reward Scale of {each['Reward Scale']}\n            {round(each['24h'], 2)} HNTs in the past 24h,\n            {round(each['Week'], 2)} HNTs in the past week,\n            {round(each['Month'], 2)} HNTs in the past month.\n\n")
+                time.sleep(0.3)
 
-    cc = 0
-    for each in data:
-        cc += 1
-        if each['Status'] == 'OFFLINE':
-            slowprint(f"\n    {cc} - {each['Name']} is currently OFFLINE\n\n")
-        else:
-            print(f"\n    {cc} - {each['Name']} is currently ONLINE with a Reward Scale of {each['Reward Scale']}\n            {round(each['24h'], 2)} HNTs in the past 24h,\n            {round(each['Week'], 2)} HNTs in the past WEEK,\n            {round(each['Month'], 2)} HNTs in the past Month.\n\n")
-            time.sleep(0.3)
-
-    slowprint(' >>>>>>> Would you like to download the data in a Excel file?\n      Yes or no ◔_◔ >>>> ')
-    answer = input().lower()
-    
-    if answer == 'yes' or answer == 'y':
-        turn_data_into_csv(data)
-        return True
-
-    return False
+        slowprint(' >>>>>>> Would you like to download these data in an Excel file?\n      Yes or No (●__●) >>>> ')
+        answer = input().lower()
+        
+        if answer == 'yes' or answer == 'y':
+            turn_data_into_csv(data)
+            return True
+    except:
+        return 'zero'
 
 print('''
 
@@ -162,18 +165,24 @@ print('''
                                                                     ██╔══██║░██║██║██║░░░░██║░░░
                                                                     ██║░░██║░██║█████║░░░░██║░░░
 
-                                                                    Welcome to Helium Mini App!!!
+                                                                    Welcome to Leo Helium App!!!
                                                                             
+
                                                                             (▀̿Ĺ̯▀̿) ₿₿₿₿₿₿
 ''')
-slowprint('''                                            This application is designed to find and compare HELIUM HOTSPOTS data in a choosen area
-                                          By simplelly entering the the address of the location you wish to seach and radius distance 
-                                        You now have access to data of all HNT hotspots you. No more speding hours comparing hexacons!!!
+slowprint('''
+                                    This application will help you locate and measure the rewards of all HNT HotSpots in the desired location.
+                                     By simply entering an address and distance; You now have access to a list of HNTs gains in the past days. 
+                                                     No more spending hours comparing hexagons on Helium Explorer!!!
                                                                                                                             
-                                                        Find out the best locations for your next HELIUM INVESTMENT 
+                                                    Find out the best locations for your next ₿₿ HELIUM investment ₿₿ 
 ''')
 while True:
     run = average(HotSpots_Data_Finder())
+    if run == 'zero':
+        time.sleep(2)
+        slowprint("\n >>>>>>> (╯° ͟ʖ °)╯┻━┻ ups!!!! There are no HNT Hotspots currently registered in this area. Try again with a farther distance...\n")
+    
     time.sleep(2)
     loop = str(input('\n >>>>>>> Would you like another search?\n >>>>>>>  '))
     loop = loop.lower()
@@ -181,7 +190,7 @@ while True:
         break
 
 slowprint('''
->>>>>>>            Thanks for using the Helium Mini App !!!!
+>>>>>>>            Thanks for using the Leo Helium App !!!!
 
 
                         ░░░░░█▐▓▓░████▄▄▄█▀▄▓▓▓▌█ 
@@ -198,4 +207,7 @@ slowprint('''
 
                                  TO THE MOON
 
+
 ''')
+
+#using last  address once put a crazy wrong address
