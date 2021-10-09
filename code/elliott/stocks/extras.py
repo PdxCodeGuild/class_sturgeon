d3c3b00e58ad886bce293a578e7394915a302bde
Elliott smith <elliott.smith2437@gmail.com>
2021-10-08 22:42:26 -0700

    mini-capstone

--- code/elliott/stocks/extras.py
+++ code/elliott/stocks/extras.py
@@ -1,5 +1,93 @@
+import requests
+import yfinance as yf
+from yahoo_fin.stock_info import get_data
+import yahoo_fin.stock_info as si
+
+
+ticker = input(">")
+
 '''
-#analysis
+def finacials(ticker):
+    quote_table = si.get_quote_table(ticker, dict_result=False)
+    pe_ratio = quote_table
+    print(pe_ratio)
+    income_statement = si.get_income_statement(ticker)
+    print(income_statement)
+'''
+
+
+def analysis(ticker):
+    analysis = requests.get("https://yh-finance.p.rapidapi.com/stock/v2/get-analysis", headers={
+        'x-rapidapi-host': "yh-finance.p.rapidapi.com",
+        'x-rapidapi-key': "3626506961msh6043bed195d39acp11b912jsna00bcc337f5c"}, params={"symbol": ticker, "region": "US"}).json()
+    buy_sell = analysis['financialData']['recommendationKey']
+    cash_flow = analysis['financialData']['freeCashflow']['fmt']
+    fifty_two_weekHigh = analysis['summaryDetail']['fiftyTwoWeekHigh']['fmt']
+    debt_to_equity = analysis['financialData']['debtToEquity']
+    previous_price = analysis['price']['regularMarketPreviousClose']
+    print(f"""
+    These are our reccomendations based on the fundamental analysis:
+    --------------------------------------------------------------------
+    {cash_flow}
+    {fifty_two_weekHigh}
+    {debt_to_equity}
+    {buy_sell}
+
+
+
+
+    3.back
+    --------------------------------------------------------------------
+     > """).lower()
+
+
+
+
+
+""")
+
+
+analysis(ticker)
+
+
+'''
+def analysis(ticker):
+    print(f" The anaylis for today is ..... {ticker}")
+
+
+def news(ticker, limit):
+    print(f" The news for today is ..... {ticker}")
+
+
+def earnings(ticker):
+    print(f" The earnings for today is ..... {ticker}")
+
+     EARNINGS PER SHARE(EPS)
+
+
+PRICE TO EARNINGS RATIO(P/E)
+PROJECTED EARNINGS GROWTH(PEG)
+. FREE CASH FLOW(FCF)
+PRICE TO BOOK RATIO(P/B)
+RETURN ON EQUITY(ROE)
+DIVIDEND PAYOUT RATIO(DPR)
+ PRICE TO SALES RATIO(P/S)
+ DIVIDEND YIELD RATIO
+ DEBT-TO-EQUITY RATIO(D/E)
+
+
+ticker = input("Input ticker symbol: ").upper()
+# PRICE TO EARNINGS RATIO (P/E)
+quote_table = si.get_quote_table(ticker, dict_result=False)
+pe_ratio = quote_table
+
+print(pe_ratio)
+
+income_statement = si.get_income_statement(ticker)
+print(income_statement)
+'''
+'''
+# analysis
 response = requests.get("https://yh-finance.p.rapidapi.com/stock/v2/get-analysis", headers={
     'x-rapidapi-host': "yh-finance.p.rapidapi.com",
     'x-rapidapi-key': "3626506961msh6043bed195d39acp11b912jsna00bcc337f5c"
@@ -7,7 +95,7 @@ response = requests.get("https://yh-finance.p.rapidapi.com/stock/v2/get-analysis
 
 print(response)
 
-#news
+# news
 import requests
 
 url = "https://yh-finance.p.rapidapi.com/news/v2/get-details"
@@ -22,5 +110,64 @@ headers = {
 response = requests.request("GET", url, headers=headers, params=querystring)
 
 print(response.text)
+
+
+
+
+
+
+
+#------------------------------------------------------------------------------------------------#
+ EARNINGS PER SHARE (EPS)
+PRICE TO EARNINGS RATIO (P/E)
+PROJECTED EARNINGS GROWTH (PEG)
+. FREE CASH FLOW (FCF)
+PRICE TO BOOK RATIO (P/B)
+RETURN ON EQUITY (ROE)
+DIVIDEND PAYOUT RATIO (DPR)
+ PRICE TO SALES RATIO (P/S)
+ DIVIDEND YIELD RATIO
+ DEBT-TO-EQUITY RATIO (D/E)
+#------------------------------------------------------------------------------------------------#
+
+
+# analysis
+response = requests.get("https://yh-finance.p.rapidapi.com/stock/v2/get-analysis", headers={
+    'x-rapidapi-host': "yh-finance.p.rapidapi.com",
+    'x-rapidapi-key': "3626506961msh6043bed195d39acp11b912jsna00bcc337f5c"
+}, params={"symbol": ticker, "region": "US"}).json()
+
+print(response)
+
+
+
+url = "https://yh-finance.p.rapidapi.com/news/v2/list"
+
+querystring = {"region": "US", "snippetCount": "28", "s": ticker}
+
+payload = "\"Pass in the value of uuids field returned right in this endpoint to load the next page, or leave empty to load first page\""
+headers = {
+    'content-type': "text/plain",
+    'x-rapidapi-host': "yh-finance.p.rapidapi.com",
+    'x-rapidapi-key': "3626506961msh6043bed195d39acp11b912jsna00bcc337f5c"
+}
+
+response = requests.request(
+    "POST", url, data=payload, headers=headers, params=querystring).json()
+
+print(response)
+
+
+
+
+
+# data = yf.Ticker(ticker)
+# print(data.info)
+
+#------------------------------------------------------------------------------------------------#
+t12_api_key = "00854453c7a841729c6197f1074e8bce"
+rapidapi_key = "3626506961msh6043bed195d39acp11b912jsna00bcc337f5c"
+poly_api_key = "2d5b7LFFuTGUowh3lmLW8M_pFQbDNmx3"
+#------------------------------------------------------------------------------------------------#
 
 '''
--- code/elliott/stocks/stocks.py
+++ code/elliott/stocks/stocks.py
@@ -1,39 +1,124 @@
-import yfinance as yf
 import requests
-#import yahoo_fin.stock_info as si
-import pandas as pd
-import json
+import yfinance as yf
+import yahoo_fin.stock_info as si
+
+
+#------------------------------------------------------API----------------------------------------------------------#
+
+
+def rec(ticker):
+    x = yf.Ticker(ticker)
+    print(x.recommendations)
+
+
+def news(ticker, limit):
+    poly_api_key = "2d5b7LFFuTGUowh3lmLW8M_pFQbDNmx3"
+    url = f"https://api.polygon.io/v2/reference/news?limit={limit}&order=descending&sort=published_utc&ticker={ticker}&apiKey={poly_api_key}"
+    news = requests.get(url).json()
+    print(news)
+
+
+def finacials(ticker):
+    quote_table = si.get_quote_table(ticker, dict_result=False)
+    pe_ratio = quote_table
+    print(pe_ratio)
+    income_statement = si.get_income_statement(ticker)
+    print(income_statement)
+
+
+def analysis(ticker):
+    analysis = requests.get("https://yh-finance.p.rapidapi.com/stock/v2/get-analysis", headers={
+        'x-rapidapi-host': "yh-finance.p.rapidapi.com",
+        'x-rapidapi-key': "3626506961msh6043bed195d39acp11b912jsna00bcc337f5c"}, params={"symbol": ticker, "region": "US"}).json()
+    buy_sell = analysis['financialData']['recommendationKey']
+    cash_flow = analysis['financialData']['freeCashflow']['fmt']
+    fifty_two_weekHigh = analysis['summaryDetail']['fiftyTwoWeekHigh']['fmt']
+    debt_to_equity = analysis['financialData']['debtToEquity']['fmt']
+    previous_price = analysis['price']['regularMarketPreviousClose']['fmt']
+
+    print(f"""
+    These are our reccomendations based on the fundamental analysis: 
+    --------------------------------------------------------------------
+    
+    {ticker}'s previous closing price was ${previous_price}. With a 52 week high of ${fifty_two_weekHigh}. 
+    Expert analysis has it rated as a {buy_sell}. 
+
+    Important metics:
+    Cash flow (+/-): {cash_flow} 
+    Debt to equity: {debt_to_equity}
+
+    --------------------------------------------------------------------
+     > """)
+#-------------------------------------------------------API---------------------------------------------------------------#
+
+
+#----------------------------------------------------user promps----------------------------------------------------------#
+def promp():
+    choice = input("""
+    Enter one of the options below or 'All' for full fundamental report.
+    --------------------------------------------------------------------
+      1. News
+      2. Analysis [...]
+      3. Reccomendations
+      4. All
+      5. Exit
+    --------------------------------------------------------------------
+     > """).lower()
+    return choice
+
+
+def promp2():
+    choice2 = input("""
+    Enter one of the options below or 'back' to return to last page.
+    --------------------------------------------------------------------
+    1.finacials
+    2.expert buy/sell
+    3.back
+
+    --------------------------------------------------------------------
+     > """).lower()
+    return choice2
+
+
+def promp3():
+    choice3 = input("""
+    1. Back
+    2. Exit
+     > """).lower()
+    return choice3
+#----------------------------------------------------user promps----------------------------------------------------------#
+
 
 ticker = input("Input ticker symbol: ").upper()
-
-data = yf.Ticker(ticker)
-
-print(data.info).json()
-
-'''
-# analysis
-response = requests.get("https://yh-finance.p.rapidapi.com/stock/v2/get-analysis", headers={
-    'x-rapidapi-host': "yh-finance.p.rapidapi.com",
-    'x-rapidapi-key': "3626506961msh6043bed195d39acp11b912jsna00bcc337f5c"
-}, params={"symbol": ticker, "region": "US"}).json()
-
-print(response)
-
-
-
-url = "https://yh-finance.p.rapidapi.com/news/v2/list"
-
-querystring = {"region": "US", "snippetCount": "28", "s": ticker}
-
-payload = "\"Pass in the value of uuids field returned right in this endpoint to load the next page, or leave empty to load first page\""
-headers = {
-    'content-type': "text/plain",
-    'x-rapidapi-host': "yh-finance.p.rapidapi.com",
-    'x-rapidapi-key': "3626506961msh6043bed195d39acp11b912jsna00bcc337f5c"
-}
-
-response = requests.request(
-    "POST", url, data=payload, headers=headers, params=querystring).json()
-
-print(response)
-'''
+# choice = promp()
+while True:
+    choice = promp()
+    if choice == "1":
+        limit = input(f"How many news articles related to {ticker}?  ")
+        news(ticker, limit)
+    elif choice == "2":
+        while True:
+            choice2 = promp2()
+            if choice2 == "1":
+                finacials(ticker)
+            elif choice2 == "2":
+                rec(ticker)
+            elif choice2 == "3":
+                break
+    elif choice == "3":
+        analysis(ticker)
+        while True:
+            choice3 = promp3()
+            if choice3 == "1":
+                break
+            elif choice3 == "2":
+                quit()
+    elif choice == "4":
+        rec(ticker),
+        analysis(ticker)
+        news(ticker, '5')
+    elif choice == "5":
+        break
+    else:
+        print(f'{choice} is not a option. ')
+        break
