0dd54c810d398cbff4157546bbae784263b2662b
liz <bean@lizs-MacBook-Pro.local>
2021-10-08 17:21:56 -0700

    lab 16, mini capstone, it works

--- code/liz/lab_16.py
+++ code/liz/lab_16.py
@@ -10,15 +10,11 @@ import datetime
 
 
 
-def all_critters (critter_list):
+def all_critters (critter_list): # simple critter name list, no extra data
     return [''.join(critter.keys()) for critter in critter_list]
   
 def new_critter (user_input, inventory_list):
     inventory_list.append(user_input)
-
-def my_critters (inventory_list):
-    for critter in inventory_list:
-        print(critter, '\n')
 
 def remaining_critters (inventory_list, critter_list):
     remaining_critters = [critter for critter in critter_list if critter not in inventory_list]
@@ -34,10 +30,22 @@ def pie_chart (inventory_list, critter_list, section):
     chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
     webbrowser.get(chrome_path).open(url)
 
+def available_now (inventory_list, critter_list):
+    current_datetime = datetime.datetime.now()
+    month = current_datetime.month
+    hour = current_datetime.hour
+    catch_of_the_day = []
+    for critter in critter_list:
+        for item in critter:
+            if month in critter[item]['month-array-northern'] and hour in critter[item]['time-array']:
+                catch_of_the_day.append(critter)
+    temp_critter_inventory = remaining_critters(inventory_list, all_critters(critter_list))
+    # print(temp_critter_inventory)
+    return [critter for critter in catch_of_the_day if ''.join(critter.keys()) in temp_critter_inventory]
 
+# -------------------------------------------------------------------------------------------------- #
 
 fish_response = requests.get("https://acnhapi.com/v1a/fish")
-#print(fish_response.json())
 # name, rarity, location, time, months(northern and southern), shadow size
 fish_list = [{fish['name']['name-USen'] : {'rarity' : fish['availability']['rarity'], 'location' : fish['availability']['location'], 'time-array' : fish['availability']['time-array'], 'month-array-northern' : fish['availability']['month-array-northern'], 'month-array-southern' : fish['availability']['month-array-southern'], 'shadow' : fish['shadow']}} for fish in fish_response.json()]
 
@@ -69,79 +77,70 @@ user_inventories = {
     'bugs' : bugs_inventory
 }
 
+# -------------------------------------------------------------------------------------- #
 
 # begin program
-# print('Welcome to the Animal Crossing Critter Catching Companion!\n')
-# while True:
-#     section = input("What critter category are you interested in working on today?\nEnter 'fish', 'sea creatures', 'bugs', or 'exit'\n")
-#     if section == 'exit':
-#         print('Goodbye')
-#         break
+print('Welcome to the Animal Crossing Critter Catching Companion!\n')
+while True:
+    section = input("What critter category are you interested in working on today?\nEnter 'fish', 'sea creatures', 'bugs', or 'exit'\n")
+    if section == 'exit':
+        print('Goodbye')
+        break
     
-    # elif section in list(critter_encylcopedia.keys()):
-    #     while True:
-    #         command = input(f"""
-    # What would you like to do?
-    #     'list' = see all {section} available
-    #     'new' = enter new {section} into your inventory
-    #     'inventory' = see your {section} inventory
-    #     'uncaught' = see what {section} you have left to catch
-    #     'chart' = see caught vs uncaught {section} chart
-    #     'other' = change critter category
-    #         """)
-    #         if command == 'list':
-    #             for critter in all_critters(critter_encylcopedia[section]):
-    #                 print(critter)
+    elif section in list(critter_encylcopedia.keys()):
+        while True:
+            command = input(f"""
+What would you like to do?
+    'list' = see all {section} available
+    'new' = enter new {section} into your inventory
+    'inventory' = see your {section} inventory
+    'uncaught' = see what {section} you have left to catch
+    'chart' = see caught vs uncaught {section} chart
+    'now' = see what uncaught critter is available to catch now
+    'other' = change critter category
+            """)
+            if command == 'list':
+                for critter in all_critters(critter_encylcopedia[section]):
+                    print(critter)
 
-    #         elif command == 'new':
-    #             while True:
-    #                 user_critter = input(f"Enter what you caught:\n").lower()
-    #                 if user_critter in all_critters(critter_encylcopedia[section]):
-    #                     new_critter (user_critter, user_inventories[section])
-    #                     print(user_inventories[section])
-    #                     another = input("Would you like to make another entry? 'yes' or 'no'\n")
-    #                     if another != 'yes':
-    #                         break
-    #                 else:
-    #                     print(f'That entry is not in the {section} list')
+            elif command == 'new':
+                while True:
+                    user_critter = input(f"Enter what you caught:\n").lower()
+                    if user_critter in all_critters(critter_encylcopedia[section]):
+                        new_critter (user_critter, user_inventories[section])
+                        print(user_inventories[section])
+                        another = input("Would you like to make another entry? 'yes' or 'no'\n")
+                        if another != 'yes':
+                            break
+                    else:
+                        print(f'That entry is not in the {section} list')
 
-    #         elif command == 'inventory':
-    #             if user_inventories[section] == []:
-    #                 print("Your inventory is empty")
-    #             else:
-    #                 for critter in user_inventories[section]:
-    #                     print(critter)
+            elif command == 'inventory':
+                if user_inventories[section] == []:
+                    print("Your inventory is empty")
+                else:
+                    for critter in user_inventories[section]:
+                        print(critter)
 
-    #         elif command == 'uncaught':
-    #             for critter in remaining_critters (user_inventories[section], all_critters(critter_encylcopedia[section])):
-    #                 print(critter)
+            elif command == 'uncaught':
+                for critter in remaining_critters(user_inventories[section], all_critters(critter_encylcopedia[section])):
+                    print(critter)
 
-    #         elif command == 'chart':
-    #             pie_chart(user_inventories[section], all_critters(critter_encylcopedia[section]), section)
+            elif command == 'chart':
+                pie_chart(user_inventories[section], all_critters(critter_encylcopedia[section]), section)
 
-    #         elif command == 'other':
-    #             break
+            elif command == 'now':
+                # {'coelacanth': {'rarity': 'Ultra-rare', 'location': 'Sea (when raining or snowing)', 'time-array': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23], 'month-array-northern': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 'month-array-southern': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 'shadow': 'Largest (6)'}} 
+                for critter in available_now(user_inventories[section], critter_encylcopedia[section]):
+                    print(critter, '\n')
 
-    #         else:
-    #             print('Entry not recognized')
-    # else:
-    #     print('Entry not recognized')
+            elif command == 'other':
+                break
 
-
-
-# add while true on line 84? change if statements to elif and else
-# work on datetime? use data to see if anything is avaiable to catch
-current_datetime = datetime.datetime.now()
-month = current_datetime.month
-hour = current_datetime.hour
-
-# print(month, hour)
-
-# for fish in fish_list:
-#     if month in fish['month-array-northern']:
-#         print(fish, '\n')
+            else:
+                print('Entry not recognized')
+    else:
+        print('Entry not recognized')
 
 
-
-
-
+# future endevours - make 'avaiable now' info more user friendly, and add suggestion for next available critter to catch if you caught all in the now section
