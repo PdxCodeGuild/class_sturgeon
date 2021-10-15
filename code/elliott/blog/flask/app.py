85f9458a7b1707dbbee22a4277ea5bfbddf7e55c
Elliott smith <elliott.smith2437@gmail.com>
2021-10-15 16:23:34 -0700

    blog lab

--- code/elliott/blog/flask/app.py
+++ code/elliott/blog/flask/app.py
@@ -1,37 +1,35 @@
+
+
 from flask import Flask, render_template
 app = Flask(__name__)
 
 
 posts = [{
-    'title': "Chillwave offal you probably haven't heard of them",
-    'author': "Blogger McBlogface",
-    'date': "October 14th, 2021",
-    'body': "Godard mlkshk ethical XOXO knausgaard taiyaki narwhal sustainable port",
+    'body': "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam est, blanditiis debitis quasi eaque recusandae sed quos consectetur nobis impedit.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam facilis placeat tempore ratione in dicta vitae?",
+    'author': "Elusive man",
+    'date': "October 02th, 2021",
+
 },
 
     {
-        'title': "Chillwave offal you probably haven't heard of them",
-        'author': "Blogger McBlogface",
-        'date': "October 14th, 2021",
-        'body': "Godard mlkshk ethical XOXO knausgaard taiyaki narwhal sustainable port",
+        'body': "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam est, blanditiis debitis quasi eaque recusandae sed quos consectetur nobis impedit.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam facilis placeat tempore ratione in dicta vitae?",
+        'author': "someone",
+        'date': "October 08th, 2021",
 },
 
     {
-        'title': "Chillwave offal you probably haven't heard of them",
-        'author': "Blogger McBlogface",
-        'date': "October 14th, 2021",
-        'body': "Godard mlkshk ethical XOXO knausgaard taiyaki narwhal sustainable port",
-},
+        'body': "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam est, blanditiis debitis quasi eaque recusandae sed quos consectetur nobis impedit.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam facilis placeat tempore ratione in dicta vitae?",
+        'author': "--aa--",
+        'date': "October 14th, 2021", },
+
 
     {
-        'title': "Chillwave offal you probably haven't heard of them",
-        'author': "Blogger McBlogface",
-        'date': "October 14th, 2021",
-        'body': "Godard mlkshk ethical XOXO knausgaard taiyaki narwhal sustainable port",
-}]
+        'body': "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam est, blanditiis debitis quasi eaque recusandae sed quos consectetur nobis impedit.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam facilis placeat tempore ratione in dicta vitae?",
+        'author': "Blogger ",
+        'date': "October 17th, 2021", }]
 
 
-@app.route('/')
+@ app.route('/')
 def index():
     return render_template('index.html', posts=posts)
 
