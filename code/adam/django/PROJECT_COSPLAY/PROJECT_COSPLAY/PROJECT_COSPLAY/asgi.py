211777e57b2c676b084b2cc6f7925f215cd1be11
Adam <65050037+Library-Interface@users.noreply.github.com>
2021-10-26 17:29:15 -0600

    getting closer to finishing our polls

---
+++ code/adam/django/PROJECT_COSPLAY/PROJECT_COSPLAY/PROJECT_COSPLAY/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for PROJECT_COSPLAY project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PROJECT_COSPLAY.settings')
+
+application = get_asgi_application()
--- code/adam/django/polls/polls/views.py
+++ code/adam/django/polls/polls/views.py
@@ -1,4 +1,31 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 def index(request):
-    return HttpResponse("Hello World!")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5] #-pub_date makes it the latest question on top. the [:5] shows only the latest 5
+    context = {'latest_question_list': latest_question_list,}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice."
+        })
+    selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    selected_choice.votes += 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,))) #need the , after question.id to let it know it's a tuple
