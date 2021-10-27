509c362de9d99baec238cd6e0ec002f95724b607
Leo <89034569+lenehrt@users.noreply.github.com>
2021-10-27 17:21:19 -0400

    Django

--- code/ian/lab15.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-response = requests.get('https://favqs.com/api/qotd', headers= {'Content-Type': 'application/json'})
-
-data = response.json()
-
-print(f"Quote - {data['quote']['body']}")
-print(f"Author - {data['quote']['author']}")
-print(f"Tags - {data['quote']['tags']}")
