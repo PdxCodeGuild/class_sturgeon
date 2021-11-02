9c75c7f273038f17b976ecd7172e19e07e6f74db
Adam <65050037+Library-Interface@users.noreply.github.com>
2021-11-02 15:31:47 -0600

    Django Lab02 URL Short Complete

--- code/adam/django/short/short/urls.py
+++ code/adam/django/short/short/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from . views import index, createshorturl, urlcreated, url_link, delete_url
+from . views import index, createshorturl, urlcreated, url_link, delete_url, delete_all
 
 app_name="short"
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path("list", urlcreated, name="urlcreated"),
     path("this_url_can_be_changed_at_will_thanks_to_the_href_in_the_urls_py_page_line_21/<str:for_the_function_in_views_from_urls>", url_link, name="reference_me_in_urls_html"),
     path('delete_item/<pk>', delete_url, name='delete_url'),
+    path('delete_all/', delete_all, name='delete_all'),
 ]
