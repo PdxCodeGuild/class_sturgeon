10bd022457169ec18db4bab09e21e34dff2f5508
Leo <89034569+lenehrt@users.noreply.github.com>
2021-11-08 19:24:41 -0500

    Lab 3 - in progress

---
+++ code/leo/DJANGO/Lab_3_Chirp/project/posts/urls.py
@@ -0,0 +1,8 @@
+polls/urls.py
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
---
+++ code/leo/DJANGO/Lab_3_Chirp/project/posts/views.py
@@ -0,0 +1,6 @@
+polls/views.py:
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
