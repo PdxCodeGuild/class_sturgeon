63abb36f31c64c47519174d8c107ca8fc630fb68
Leo <89034569+lenehrt@users.noreply.github.com>
2021-11-16 15:30:26 -0500

    l

--- code/moblab_js_group1/todo.js
+++ code/moblab_js_group1/todo.js
@@ -1,14 +1,35 @@
-const todoInput = document.querySelector("todo-input")
-const todoButton = document.querySelector("todo-submit")
-const todoList = document.querySelector("todo-list")
+const todoInput = document.querySelector(".todo-input")
+const todoButton = document.querySelector(".todo-submit")
+const todoList = document.querySelector(".todo-list")
+const completeList = document.querySelector(".complete-list")
 
-todoButton.addEventListener('click', function addTodo() {
+todoButton.addEventListener('click', function () {
     const todoDiv = document.createElement("div")
     todoDiv.classList.add("todo")
     const newTodo = document.createElement("li")
     newTodo.classList.add("list-item")
     newTodo.innerText = todoInput.value
+    
+
+    let deletebutton = document.createElement("button")
+    deletebutton.innerText= 'Delete'
+    deletebutton.addEventListener('click', function(){
+        todoDiv.remove()
+    })
+
+    let completebutton = document.createElement("button")
+    completebutton.innerText= 'Complete'
+    completebutton.addEventListener('click', function(){
+        todoDiv.appendChild(completeList)
+        completebutton.remove()
+    })
+
+
     todoDiv.appendChild(newTodo)
+    todoDiv.appendChild(deletebutton)
+    todoDiv.appendChild(completebutton)
+
+    todoList.appendChild(todoDiv)
+
+
 })
-
-
