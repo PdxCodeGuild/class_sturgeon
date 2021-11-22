ecaba4e6777a9f6df1e9833dbbecf00d28ffc22e
liz <bean@lizs-MacBook-Pro.local>
2021-11-22 15:17:16 -0800

    lab 5

--- code/liz/javascript_labs/lab_05/site.js
+++ code/liz/javascript_labs/lab_05/site.js
@@ -1,9 +1,23 @@
-const vm = new Vue({
+Vue.component('list-item', {
+    props: ['quote'],
+    template: `
+    <li>
+        {{ quote.body }} <br>&emsp;- {{ quote.author }}
+    </li>
+    `
+})
+
+
+new Vue({
     el: '#app',
     data: {
         quotes: {},
         selectedType: '',
         userInput: '',
+        results: {},
+        page: 1,
+        lastUserInput: '',
+        lastSelectedType: '',
     },
     methods: {
         getQuotes: function() {
@@ -22,13 +36,34 @@ const vm = new Vue({
                 method: 'get',
                 url: 'https://favqs.com/api/quotes/',
                 params: {
-                    filter: 'this.userInput',
-                    type: 'selectedType}'
+                    filter: this.userInput,
+                    type: this.selectedType,
+                    page: this.page
                 },
                 headers: {
                     Authorization: `Token token="${apiKey}"`
                 }
+            }).then(response => {
+                this.results = response.data
             })
+            this.lastUserInput = this.userInput
+            this.lastSelectedType = this.selectedType
+            this.userInput = ''
+            this.selectedType = ''
+        },
+        nextPage: function() {
+            this.page++
+            console.log(this.page)
+            this.userInput = this.lastUserInput
+            this.selectedType = this.lastSelectedType
+            this.requestedQuotes()
+        },
+        backPage: function() {
+            this.page--
+            console.log(this.page)
+            this.userInput = this.lastUserInput
+            this.selectedType = this.lastSelectedType
+            this.requestedQuotes()
         }
     },
     created: function() {
