99e4743df717a616dba5d5f39a8bc6da03055a44
Ian <iandavis6481@gmail.com>
2021-11-22 10:48:43 -0800

    Lab 5 complete

--- code/ian/vue/Lab 5/js/site.js
+++ code/ian/vue/Lab 5/js/site.js
@@ -6,13 +6,12 @@ Vue.component('quote-item',{
     },
     props: ['quote'],
     template: `
-    <div>
+    <div class="quote">
         <h3>{{ quote.body }}</h3>
         <p>{{ quote.author }}</p>
         <p v-if="quote.tags.length"><u>Tags:</u> 
         <li v-for="tag in quote.tags">{{ tag }}</li>
         </p>
-        <hr>
         <br>
     </div>
     `,
@@ -24,8 +23,10 @@ new Vue ({
     data: {
         message: "Quote time",
         search_text: '',
+        next_search: '',
         type: 'author',
         page: 1,
+        next_type: '',
         qod: {},
     },
     methods: {
@@ -57,10 +58,46 @@ new Vue ({
                 this.qod = (response.data)
                 
             })
+                this.next_search = this.search_text
+                this.next_type = this.type
                 this.search_text = '' 
                 this.type = 'author'
+                
             },
-        
+        new_page: function () {
+            this.page ++
+            axios({
+                method: "get",
+                url: "https://favqs.com/api/quotes",
+                headers: {
+                    "Authorization": 'Token token="a5cc4f9c190f7f0149e309a86572efbb"'
+                },
+                params: {
+                    "filter" : this.next_search,
+                    "type": this.next_type,
+                    "page": this.page
+                }
+            }).then((response) => {
+                this.qod = (response.data)
+            })
+        },
+        last_page: function () {
+            this.page --
+            axios({
+                method: "get",
+                url: "https://favqs.com/api/quotes",
+                headers: {
+                    "Authorization": 'Token token="a5cc4f9c190f7f0149e309a86572efbb"'
+                },
+                params: {
+                    "filter" : this.next_search,
+                    "type": this.next_type,
+                    "page": this.page
+                }
+            }).then((response) => {
+                this.qod = (response.data)
+            })
+        }
         
     },
     created: function (){
