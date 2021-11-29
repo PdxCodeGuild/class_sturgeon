e6ab883faaafbebfcf3c05e81829ec55db596960
Merritt Lawrenson <merrittlawrenson@gmail.com>
2021-11-29 14:49:14 -0800

    drf permissions

--- code/merritt/21-sept/drf-blog-21sept/api/urls.py
+++ code/merritt/21-sept/drf-blog-21sept/api/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
-from .views import PostList, PostDetail
+from .views import PostViewSet
 
-urlpatterns = [
-    path('<int:pk>/', PostDetail.as_view()),
-    path('', PostList.as_view()),
-]
+router = DefaultRouter()
+router.register('posts', PostViewSet, basename='posts')
+
+urlpatterns = router.urls
+
+
+# from .views import PostList, PostDetail
+
+# urlpatterns = [
+#     path('<int:pk>/', PostDetail.as_view()),
+#     path('', PostList.as_view()),
+# ]
--- code/merritt/21-sept/drf-blog-21sept/api/views.py
+++ code/merritt/21-sept/drf-blog-21sept/api/views.py
@@ -1,12 +1,20 @@
-from rest_framework import generics
+from rest_framework import generics, permissions, viewsets
 
 from posts.models import Post
 from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 
-class PostList(generics.ListCreateAPIView):
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthorOrReadOnly]
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+# class PostList(generics.ListCreateAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+#     permission_classes = [IsAuthorOrReadOnly]
+
+# class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+#     permission_classes = [IsAuthorOrReadOnly]
