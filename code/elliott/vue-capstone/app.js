fe84360338f42ddb57692866b98091de01c44248
Elliott smith <elliott.smith2437@gmail.com>
2021-11-29 12:32:34 -0800

    vue-capstone

--- code/elliott/vue-capstone/app.js
+++ code/elliott/vue-capstone/app.js
@@ -1,24 +1,57 @@
 const vm = new Vue({
-    el: '#app',
+    el: '#weatherApp',
     data: {
-        weather: {},
-        city: ''
+        weatherReport: {},
+        forecastReport: {},
+        city: '',
+
     },
+
     methods: {
-        
 
         getWeather: function() {
             axios({
                 method: 'get',
-                url: '',
+                url: 'http://api.openweathermap.org/data/2.5/weather',
                 params: {
                     q: this.city,
-                    appid: "549e6339397ac70ebf57336256bf4625"
+                    appid: "549e6339397ac70ebf57336256bf4625",
+                    units: 'imperial',
                 },
+            }).then(response => {this.weatherReport = response.data
+                this.getForecast()
+            })
                 
-                
-            }).then(response => this.results = response.data)
         },
+
+        getForecast: function(){
+            axios({
+                method: 'get',
+                url: 'https://api.openweathermap.org/data/2.5/onecall',
+                params: {
+                    lat: this.weatherReport.coord.lat,
+                    lon: this.weatherReport.coord.lon,
+                    exclude: ['current','minutely','hourly'
+                ],                    
+                appid: "549e6339397ac70ebf57336256bf4625",
+                    units: 'imperial',
+                },
+            }).then(response => this.forecastReport = response.data)
+        },
+
+        dateBuilder: function() {
+            let d = new Date();
+            let months = ["January", "February", "March", "April", "May","June", "July",
+        "August", "September", "October", " November", "December"];
+            let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+            let day = days[d.getDay()];
+            let date = d.getDate();
+            let month = months[d.getMonth()]
+            let year = d.getFullYear()
+
+            return `${day} ${date} ${month} ${year}`
+        }
 
         
     },
