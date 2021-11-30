74d0e4ddcefa034ea601ab7e81a1e4818cda41d3
Merritt Lawrenson <merrittlawrenson@gmail.com>
2021-11-30 15:43:30 -0800

    vue and drf

--- code/merritt/21-sept/drf-blog-21sept/api/urls.py
+++ code/merritt/21-sept/drf-blog-21sept/api/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import PostViewSet
+from .views import PostViewSet, UserViewSet, CurrentUserView
 
 router = DefaultRouter()
 router.register('posts', PostViewSet, basename='posts')
+router.register('users', UserViewSet, basename='users')
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path('currentuser/', CurrentUserView.as_view())
+]
 
 
 # from .views import PostList, PostDetail
