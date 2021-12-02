3a23887bd1bcfca60d459382707eb3479c4aa9e7
Adam <65050037+Library-Interface@users.noreply.github.com>
2021-12-01 17:18:48 -0700

    Pokedex with CSS all done

--- code/adam/django/pokedex-main/apis/serializers.py
+++ code/adam/django/pokedex-main/apis/serializers.py
@@ -1,23 +1,23 @@
 from rest_framework import serializers
 from pokemon import models
+from django.contrib.auth import get_user_model
 
 
 class NestedPokemonSerializer(serializers.ModelSerializer):
     class Meta:
+        model = models.Type
         fields = (
-            'id',
-            'number',
-            'name',
-            'height',
-            'weight',
-            'image_front',
-            'image_back',
-            'caught_by',
-            'types',
+            'type',
         )
-        model = models.Pokemon
+
+class NestedUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('username',)
 
 class PokemonSerializer(serializers.ModelSerializer):
+    pokemon_type = NestedPokemonSerializer(many=True, read_only=True, source='types')
+    trainer = NestedUserSerializer(many=True, read_only=True, source='caught_by')
     class Meta:
         fields = (
             'id',
@@ -28,6 +28,12 @@ class PokemonSerializer(serializers.ModelSerializer):
             'image_front',
             'image_back',
             'caught_by',
-            'types',
+            'trainer',
+            'pokemon_type',
         )
         model = models.Pokemon
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username', 'caught')
--- code/adam/django/pokedex-main/apis/urls.py
+++ code/adam/django/pokedex-main/apis/urls.py
@@ -1,8 +1,25 @@
 from django.urls import path
 
-from .views import ListPokemon, DetailPokemon
+from .views import PokemonViewSet, CurrentPokemonView, CurrentUserView, UserViewSet
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    path('', ListPokemon.as_view()),
-    path('<int:pk>/', DetailPokemon.as_view()),
+
+router = DefaultRouter()
+router.register('pokemon', PokemonViewSet, basename='pokemon')
+router.register('users', UserViewSet, basename='users')
+
+urlpatterns = router.urls + [
+    path('currentuser/', CurrentUserView.as_view()),
+    path('currentpokemon/', CurrentPokemonView.as_view())
 ]
+
+
+
+
+
+
+
+# urlpatterns = [
+#     path('', ListPokemon.as_view()),
+#     path('<int:pk>/', DetailPokemon.as_view()),
+# ]
