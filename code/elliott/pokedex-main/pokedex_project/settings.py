0b1aad7fb37875d5c0440c161e939ab161d544fc
Elliott smith <elliott.smith2437@gmail.com>
2021-12-07 09:58:19 -0800

    pokedex lab

--- code/elliott/pokedex-main/pokedex_project/settings.py
+++ code/elliott/pokedex-main/pokedex_project/settings.py
@@ -142,5 +142,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
+      'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
