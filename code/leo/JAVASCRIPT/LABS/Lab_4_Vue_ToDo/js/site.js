d8d0cbfd74b036d7260363b43633be4d02d9f4c9
lenehrt <leonardo@lenehrt.com>
2021-12-07 18:22:26 -0500

    pushing updated

--- code/leo/JAVASCRIPT/LABS/Lab_4_Vue_ToDo/js/site.js
+++ code/leo/JAVASCRIPT/LABS/Lab_4_Vue_ToDo/js/site.js
@@ -1,37 +1,37 @@
 var app = new Vue({
     
-        el: '#app',
-        
-        data: {
-          newTodo: '',
-          todoList: [],
-        },
-        
-        computed: {
-          memoryToDos() {
-            return this.todoList;
-          },
-        },
-        
-        methods: {
+    el: '#app',
+    
+    data: {
+      newTodo: '',
+      todoList: [],
+    },
+    
+    computed: {
+      memoryToDos() {
+        return this.todoList;
+      },
+    },
+    
+    methods: {
+      
+      addItem: function () {
+        this.todoList.push( {
           
-          addItem: function () {
-            this.todoList.push( {
-              
-              id: this.todoList.length + 1,
-              name: this.newTodo,
-              completed: false,
-            });
-            
-            this.newTodo = '';
-          },
+          id: this.todoList.length + 1,
+          name: this.newTodo,
+          completed: false,
+        });
+        
+        this.newTodo = '';
+      },
 
-          removeTodo: function ( each ) {
-            this.todoList.splice( this.todoList.indexOf(each), 1);
-          },
+      removeTodo: function ( each ) {
+        this.todoList.splice( this.todoList.indexOf(each), 1);
+      },
 
-          completedTodo: function ( each ) {
-            each.completed = true;
-          },
-        },
-      } );
+      completedTodo: function ( each ) {
+        each.completed = true;
+      },
+    },
+  } );
